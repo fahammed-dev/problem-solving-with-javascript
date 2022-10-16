170b7c6a8b69e1c606369bea7b70b66f22ff16a4
faysalcsediu <faysal.csediu@gmail.com>
2022-10-16 22:58:18 +0600

    FizzBuzz solved

--- problemNo-2/solution.js
+++ problemNo-2/solution.js
@@ -1,2 +1,19 @@
-const str = ['I', 'Love', 'my country', 'very', 'much'];
-console.log(str[1].length);
+module.exports = { 
+    //param A : integer
+    //return a array of strings
+       fizzBuzz : function(A){
+           let array = [];
+           for (let i = 1; i <= A; i++) {
+           if (i % 3 === 0 && i % 5 === 0) {
+               array.push('FizzBuzz');
+           } else if (i % 3 === 0) {
+               array.push('Fizz');
+           } else if (i % 5 === 0) {
+               array.push('Buzz');
+           } else {
+               array.push(`${i}`);
+           }
+       }
+       return array;
+       }
+   };
