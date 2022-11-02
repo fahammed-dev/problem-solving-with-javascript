f9377fe2d1487f57de906dfa2790711d2130c84a
faysalcsediu <faysal.csediu@gmail.com>
2022-11-02 12:05:34 +0600

    Counting sort 2 solved

---
+++ problemNo-11/solution.js
@@ -0,0 +1,15 @@
+const numbers = [44, 99, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function countingSort(arr) {
+    const length = arr.length;
+    for (let i = 0; i < length; i++) {
+        for (let j = i + 1; j < length; j++) {
+            if (arr[i] > arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
